81290819100ad2a0f5db33466da70317d8f3ac51
Sebastian Lindberg <seb.lindberg@gmail.com>
2017-12-18 09:55:41 +0100

    Worked some more on the display drivers

--- include/drivers/display.h
+++ include/drivers/display.h
@@ -14,6 +14,29 @@
   (DISPLAY__WIDTH * DISPLAY__HEIGHT)
 #define DISPLAY__BUFFER_SIZE                      (2 * DISPLAY__NUM_PIXELS)
 
+#define DISPLAY__T                                (131)
+#define DISPLAY__BIT_DEPTH                        (8)
+#define DISPLAY__PERIOD                                   \
+  (((1 << DISPLAY__BIT_DEPTH) - 1) * DISPLAY__T)
+
+#define DISPLAY__COMP_BIT_0                       (DISPLAY__T)
+#define DISPLAY__COMP_BIT_1                               \
+  (DISPLAY__COMP_BIT_0 + 2 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_2                               \
+  (DISPLAY__COMP_BIT_1 + 4 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_3                               \
+  (DISPLAY__COMP_BIT_2 + 8 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_4                               \
+  (DISPLAY__COMP_BIT_3 + 16 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_5                               \
+  (DISPLAY__COMP_BIT_4 + 32 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_6                               \
+  (DISPLAY__COMP_BIT_5 + 64 * DISPLAY__T)
+#define DISPLAY__COMP_BIT_7                               \
+  (DISPLAY__COMP_BIT_6 + 128 * DISPLAY__T)
+
+#define DISPLAY__LE_bm                           PIN2_bm
+#define DISPLAY__OE_bm                           PIN0_bm
 
 /* Data Types --------------------------------------------------------------- */
 
--- src/drivers/display.c
+++ src/drivers/display.c
@@ -1,4 +1,6 @@
 #include <drivers/display.h>
+#include <drivers/uspi/device.h>
+#include <avr/interrupt.h>
 
 /* Macros ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
 
@@ -8,6 +10,9 @@
 
 static inline void
   swap_buffers();
+  
+static inline void
+  latch();
 
 
 /* Global Variables ––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
@@ -22,6 +27,21 @@ display__init()
 {
   display.read_buffer  = display.buffer;
   display.write_buffer = display.buffer + DISPLAY__BUFFER_SIZE;
+  
+  /* Setup the timer */
+  PR.PRPC &= ~PR_TC4_bm;
+  TCC4.PER = DISPLAY__PERIOD;
+  
+  TCC4.INTCTRLA = TC45_OVFINTLVL_HI_gc;
+  TCC4.INTCTRLB = TC45_CCBINTLVL_HI_gc | TC45_CCAINTLVL_HI_gc;
+  
+  TCC4.CTRLA = TC45_CLKSEL_DIV1_gc;
+  
+  /* Setup the port */
+  PORTD.DIRSET = DISPLAY__LE_bm | DISPLAY__OE_bm;
+  
+  /* Enable high level interrupts */
+  PMIC.CTRL |= PMIC_HILVLEN_bm;
 }
 
 void
@@ -30,4 +50,28 @@ swap_buffers()
   uint8_t *tmp         = display.read_buffer;
   display.read_buffer  = display.write_buffer;
   display.write_buffer = tmp;
+}
+
+void
+latch()
+{
+  //PORTD.OUTSET = DISPLAY__LE_bm;
+  //PORTD.OUTCLR = DISPLAY__LE_bm;
+  VPORT2.OUT |= DISPLAY__LE_bm;
+  VPORT2.OUT &= ~DISPLAY__LE_bm;
+}
+
+ISR(TCC4_OVF_vect)
+{
+  
+}
+
+ISR(TCC4_CCA_vect)
+{
+  
+}
+
+ISR(TCC4_CCB_vect)
+{
+  latch();
 }
